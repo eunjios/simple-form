d9a734f8de313b3815f896144af6431554c05d03
Eunji <eeunjiilee@gmail.com>
2023-10-13 21:21:39 +0900

    design: change ui
    - 인풋 상태 아이콘
    - 로직 코드는 조금 정리할 필요 있음

--- src/hooks/use-form.js
+++ src/hooks/use-form.js
@@ -1,18 +1,35 @@
 import { useState } from 'react';
+import {
+  BsCheck,
+  BsExclamationTriangleFill,
+} from 'react-icons/bs';
 
 const useForm = (regex) => {
   const [enteredInput, setEnteredInput] = useState('');
   const [isTouched, setIsTouched] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
   const isEnteredValid = regex.test(enteredInput);
   const isInputInvalid = !isEnteredValid && isTouched;
+
+  let inputIcon = null;
+  if (isTouched && isEnteredValid) {
+    inputIcon = <BsCheck />;
+  } else if (isTouched && !isEnteredValid) {
+    inputIcon = <BsExclamationTriangleFill />;
+  }
 
   const inputChangeHandler = (event) => {
     setEnteredInput(event.target.value);
   };
 
-  const inputBlurHandler = (event) => {
+  const inputFocusHandler = () => {
+    setIsFocused(true);
+  };
+
+  const inputBlurHandler = () => {
     setIsTouched(true);
+    setIsFocused(false);
   };
 
   const cleanUp = () => {
@@ -24,7 +41,10 @@ const useForm = (regex) => {
     enteredInput,
     isEnteredValid,
     isInputInvalid,
+    isFocused,
+    inputIcon,
     inputChangeHandler,
+    inputFocusHandler,
     inputBlurHandler,
     cleanUp,
   };
